4c351c6d7f4d299030e3fb10ad8b0b2d9dff1ddd
StanislavMakhnytskyi <makhnitskiy.stanislav@gmail.com>
2022-06-19 19:37:50 +0200

    (chore): initial commit

---
+++ src/theme.ts
@@ -0,0 +1,17 @@
+import {createTheme} from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: 'rgba(0, 0, 0, 0.87)', // #000000
+    },
+    secondary: {
+      main: 'rgb(54, 130, 255)', // #3682ff
+    },
+    error: {
+      main: 'rgb(255,23,68)', // #ff1744
+    },
+  },
+});
+
+export default theme;
